324275bab54b12d3bd2f668f96aa00d8631add69
SittikronNon <sittikron.lamulsab@gmail.com>
2022-07-25 01:11:47 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className='container'>
+            <div className='header-con'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
