b36ce01080527eb47e7e70661c725e0f033a11ff
SittikronNon <sittikron.lamulsab@gmail.com>
2022-07-25 01:58:21 +0700

    Learning use state
    create some header components and hook with responsive button for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FiCode, FiMenu, FiX } from 'react-icons/fi';
+import './Header.css'
 
 function Header() {
+
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <div className='header'>
-        <div className='container'>
-            <div className='header-con'>
-                
-            </div>
+      <div className='container'>
+        <div className='header-con'>
+          <div className='logo-container'>
+            <a href='#'>ReDev <FiCode /></a>
+          </div>
+          <ul className='menu'>
+            <li className='menu-link'>
+              <a href='#'>ABOUT</a>
+            </li>
+            <li className='menu-link'>
+              <a href='#'>CONTACT</a>
+            </li>
+            <li className='menu-link'>
+              <a href='#'>BLOG</a>
+            </li>
+          </ul>
+          <div className='mobile-menu' onClick={handleClick}>
+            {click ? (
+              <FiX />
+            ): <FiMenu />}
+          </div>
         </div>
+      </div>
     </div>
   )
 }
